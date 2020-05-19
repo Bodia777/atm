0010f5f367562d8ff212d0ebd1d2c691a6eecc16
Bogdan Kmet <56129358+Bodia777@users.noreply.github.com>
2020-05-20 01:58:46 +0300

    angular-material theme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Atm
-Front-end
+# Front-end
 This project-front-end was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.4.
 
 ## Development server
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AdminModule } from './modules/admin/admin.module';
+import { WrapperComponent } from './components/common/wrapper/wrapper.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
